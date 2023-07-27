9a5df2a966d207d25c7e83c5c15a280b2c44e070
ProMargo <123411022+ProMargo@users.noreply.github.com>
2023-07-27 13:44:18 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+import telebot
+from extension import *
+from utils import *
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start', 'help'])
+def help(message: telebot.types.Message):
+    text = 'Чтобы бот вам помог введите команду:\n<имя валюты> <в какую валюту перевести> <количество валюты> \nЧтобы увидеть список доступных валют: /values'
+    bot.reply_to(message, text)
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for key in keys.keys():
+        text = '\n'.join((text, key, ))
+    bot.reply_to(message, text)
+
+@bot.message_handler(content_types=['text', ])
+def convert(message: telebot.types.Message):
+    try:
+        values = message.text.split(' ')
+        if len(values) != 3:
+            raise ConvertionException('Неправильное количество параметров.')
+
+        quote, base, amount = values
+        total_base = CryptoConverter.get_price(quote, base, amount)*float(amount)
+    except ConvertionException as e:
+        bot.reply_to(message, f'Ошибка пользователя.\n{e}')
+    except Exception as e:
+        bot.reply_to(message, f'Не удалось обработать команду\n{e}')
+    else:
+        text = f'Цена {amount} {quote} в {base} - {total_base}'
+        bot.send_message(message.chat.id, text)
+
+bot.polling()
---
+++ extension.py
@@ -0,0 +1,8 @@
+TOKEN = '5888125102:AAEOjkSeR_Dk2q5uru51dqpEuuCLR02yG4g'
+
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'евро': 'EUR'
+}
